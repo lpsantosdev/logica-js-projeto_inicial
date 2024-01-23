890037c54b13d86cac8f91e4ce4b5596f103709a
Leandro Pessoa dos Santos <lsantos.bkp@gmail.com>
2024-01-23 20:47:59 -0300

    Update in app.js
    - Some changes in the variable names;
    - New features added using dynamic variables content.

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 alert("Welcome to the game: The Secret Number!");
-let secretNumber = 33;
-let typedNumber;
-let tryes = 1;
-while (typedNumber != secretNumber) {
-    typedNumber = prompt("Choose a number between 1 and 50 and type below:");
-    if(typedNumber == secretNumber){
-    alert (`Congratulations! You got it, ${secretNumber} is the secret number! You needed ${tryes} attempt(s)`);
+let varMaxNumber = 100;
+let varSecretNumber = parseInt(Math.random() *varMaxNumber +1);
+let varTypedNumber;
+let varTryes = 1;
+let varTryWord;
+console.log(varSecretNumber);
+while (varTypedNumber != varSecretNumber) {
+    varTypedNumber = prompt(`Choose a number between 1 and ${varMaxNumber} and type below:`);
+    if(varTypedNumber == varSecretNumber){
+        break;
     }
     else{
-        if (typedNumber < secretNumber) {
+        if (varTypedNumber < varSecretNumber) {
         alert("The number inputted is smaller than the secret number, try again!");
         }
         else {
         alert("The number inputted is greater than the secret number, try again!");
         }
     }
-    tryes++;
+    varTryes++;
 }
+varTryWord = varTryes == 1? "attempt":"attempts";
+alert (`Congratulations! You got it, ${varSecretNumber} is the secret number and you needed ${varTryes} ${varTryWord} to find out!`);
 
